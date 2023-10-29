758e5f4b9410ed76af1839a9c494d43983f56684
sc-katu <107098571+sc-katu@users.noreply.github.com>
2023-10-29 13:05:48 +0600

    Update main_app.py

--- main_app.py
+++ main_app.py
@@ -69,7 +69,7 @@ def main_page():
 # Define your second page content
 
 def second_page():
-    st.title("Прогнозирование. Горчак")
+    st.title("Прогнозирование. Горчак ползучий")
     # Create a Markdown table
     markdown_table = """
     | Наименование предиктора               | Описание                                        |
@@ -162,7 +162,7 @@ def third_page():
     Y = intercept + coeff_prec * x1_precavr + coeff_tmax * x2_tmax
     st.write(f"Модель развития капустной моли = {Y:.2f}")
 def fourth_page():
-    st.title("Прогнозирование. Почвообитающие")
+    st.title("Прогнозирование. Почвообитающие вредите (щелкуны, чернотелки)")
     # Create a Markdown table
     markdown_table = """
     | Наименование предиктора               | Описание                                    |
@@ -194,7 +194,7 @@ def fourth_page():
     Y = intercept + coeff_dd30 * x1_dd30d + coeff_prec7d * x2_prec7d+coeff_tavr*x3_tavr
     st.write(f"Модель развития почвообитающих = {Y:.2f}")
     
-page = st.sidebar.selectbox("Вредители", ("Септориоз", "Горчак","Капустная моль", "Почвообитающие"))
+page = st.sidebar.selectbox("Вредители", ("Септориоз", "Горчак ползучий","Капустная моль", "Почвообитающие вредители (щелкуны, чернотелки)"))
 content = st.container()
 if page == "Септориоз":
     main_page()
